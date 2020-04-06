d9710be8f8c08edc8c3413b0bf1a2bbeea92bd1d
masoliman <mark.soliman@booking.com>
2020-04-06 20:53:11 +0200

    first commit

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+
+import Rating from '@material-ui/lab/Rating';
+
+
+export default class ResturantCard extends Component {
+
+
+  onClick= (e) =>{
+    e.preventDefault();
+    this.props.clickedRestuant();
+  };
+
+
+    render() {
+        return(
+
+<div>
+<div  onClick={this.onClick}>
+            <Grid item sm={12}>
+            <h3 style={{fontStyle:"italic",fontFamily:"initial"}}>{this.props.item.name}</h3>
+
+           {this.props.item.img !=null? <img src={this.props.item.img} alt="" style={{width:"100%",height:"160px"}}/>:null}
+
+            <Rating name="simple-controlled" value={this.props.item.avgrate} readOnly  size="large"/>
+
+            <Divider light/>
+            </Grid>
+            </div>
+</div>
+
+        )
+    }
+}

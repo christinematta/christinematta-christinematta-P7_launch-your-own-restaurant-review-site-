7e1a95d7a458c4ca1bee5b0c0581688476fe6ca2
christinematta <christine.onsi@gmail.com>
2020-05-01 20:11:02 +0200

    Updates

---
+++ precache-manifest.c1a59c59dd6c5531a656eb6d3fa45ab1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1dc53027dcac7b32d9854efecbace8",
+    "url": "/P7_launch-your-own-restaurant-review-site/index.html"
+  },
+  {
+    "revision": "070561736f785f4a13bf",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "ca93b9cb9a2f447bec77",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/css/main.871f99b5.chunk.css"
+  },
+  {
+    "revision": "070561736f785f4a13bf",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/js/2.be448ec2.chunk.js"
+  },
+  {
+    "revision": "2b8cc04d3b239d52c1e995484cbcd9e3",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/js/2.be448ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca93b9cb9a2f447bec77",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/js/main.9e4e79ad.chunk.js"
+  },
+  {
+    "revision": "594a4f767617984bdf3a",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/js/runtime-main.be087fa9.js"
+  },
+  {
+    "revision": "5272c3eafc955cb06fbdf417048efd6c",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/media/logo.5272c3ea.png"
+  },
+  {
+    "revision": "5489b72f6a4c35c20dccd676907dfd0f",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/media/restmarker.5489b72f.png"
+  },
+  {
+    "revision": "e0c8f2cf9d5d896268e6141ec0460b04",
+    "url": "/P7_launch-your-own-restaurant-review-site/static/media/search-icon.e0c8f2cf.png"
+  }
+]);
